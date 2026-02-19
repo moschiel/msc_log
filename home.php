0be1eac1b5472012f13b16f34d4ee58f96b7d316
Roger da Silva Moschiel <roger.moschiel.ext@sascar.com.br>
2026-02-19 19:17:35 -0300

    blaus

--- home.php
+++ home.php
@@ -1,5 +1,5 @@
 <?php
-define('APP_VERSION', '20260219-190921');
+define('APP_VERSION', '20260219-191651');
 require_once __DIR__ . '/app/bootstrap.php';
 require_once __DIR__ . '/app/utils.php';
 require_once __DIR__ . '/app/controller.php';
