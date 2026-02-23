4d6ee06b0803815cc876df5afc6a708ea8e60a80
Roger da Silva Moschiel <roger.moschiel.ext@sascar.com.br>
2026-02-23 16:56:13 -0300

    blays

--- home.php
+++ home.php
@@ -1,5 +1,5 @@
 <?php
-define('APP_VERSION', '20260219-191651');
+define('APP_VERSION', '20260223-165400');
 require_once __DIR__ . '/app/bootstrap.php';
 require_once __DIR__ . '/app/utils.php';
 require_once __DIR__ . '/app/controller.php';
